a16a7de33050e5dcce17cd4afcf1d9e7c61f4715
Saad Ibne Omar <86186495+BadgerSIO@users.noreply.github.com>
2023-03-04 17:18:01 +0600

    product page complete

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hi is it poppins</h1>
+    <main className="w-full px-5 py-5">
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg">Welecome {"Airpul"} 👋</h1>
+        <select className="select select-bordered w-36 ">
+          <option value={"today"}>Today</option>
+          <option value={"yesterday"}>Yesterday</option>
+          <option value={"lastMonth"}>Last Month</option>
+        </select>
+      </div>
     </main>
   );
 }
--- app/products/page.js
+++ app/products/page.js
@@ -3,9 +3,10 @@ import ProductCards from "./ProductCards";
 
 const Products = async () => {
   const res = await fetch(
-    "https://product-management-server-omega.vercel.app/orders"
+    "https://product-management-server-omega.vercel.app/b2bproducts"
   );
   const data = await res.json();
+  console.log(data);
   return (
     <section className="w-full px-5">
       <div className="flex justify-between my-5">
@@ -15,8 +16,8 @@ const Products = async () => {
         </button>
       </div>
       <div className="grid grid-cols-1 ">
-        {data?.map((order) => (
-          <ProductCards key={order._id} order={order} />
+        {data?.map((product) => (
+          <ProductCards key={product._id} product={product} />
         ))}
       </div>
     </section>
